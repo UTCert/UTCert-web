6973e2178237eef6b3604463ff98929263a116cc
Minh Le <txbach.11@gmail.com>
2026-06-14 11:56:42 +0200

    up

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import Header from "@/containers/Header/header.component";
 import WhatUTCert from "@/containers/WhatUTCert/what_utcert.component";
 import styles from "../styles/Overview.module.css";
 import Features from "@/containers/Features/features.component";
-import AboutUs from "@/components/AboutUs";
 import HowToUse from "@/containers/HowToUse/how_to_use.component";
 
 function Overview() {
@@ -22,7 +21,6 @@ function Overview() {
                 <Features />
                 <HowToUse />
                 <Cta />
-                <AboutUs />
                 <Footer />
             </div>
         </>
